2358786f80a1fe23adec5bd9b98d4788a053c765
lynnlemontree <evilandsimple@163.com>
2018-05-30 22:09:34 +0800

    add wrapper function

---
+++ decoratorFunc.js
@@ -0,0 +1,22 @@
+//If we have a cpu insentive job we want cache the result so we won't waste the cpu
+
+function hardJob(x){
+    //suppose this is time-cosuming
+    return x;
+}
+
+//so next we want cache the result , so for same x, we get same result.
+function decoratorFunc(func){
+    let result = new Map();
+    //start decorate!!
+    return function(x){
+	if (result.has(x){
+	    return result.get(x);
+	}
+	let value = func(x);
+	result.set(x,value);
+	return result;
+    }
+}
+let decoratedFunc = decoratorFunc(hardJob)
+decoratedFunc(1)
